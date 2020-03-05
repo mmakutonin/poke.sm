4c7134b5c7859660e80adc076cb43ebd36246e11
mmakutonin <michael.makutonin@gmail.com>
2020-03-05 14:28:09 -0500

    Created template for day 1 project.

---
+++ pokesm-v1-incomplete/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='w3-container w3-row'>
+        Hello from React!
+      </div>
+    )
+  }
+}
+
+export default App;
